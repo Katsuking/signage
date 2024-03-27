783341ac2a64d0793d32d2cfdda41c670e1c050a
tatsunoshin <tatsujack@icloud.com>
2024-03-27 11:53:57 +0900

    アプリ終了

--- goodbyeUsb.ps1
+++ goodbyeUsb.ps1
@@ -1,2 +1,8 @@
 Add-Type -Assembly System.Windows.Forms
 [System.Windows.Forms.MessageBox]::Show("Good Bye Usb!!", "ハロー")
+
+1	#メモ帳を終了
+2	$pro = Get-process notepad
+3	if ( $pro -ne $null ) {
+4	Stop-process -name notepad
+5	}
