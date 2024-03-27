8d9094fc80fe37c0fc0d075d8cc6d56f17ca8db3
tatsunoshin <tatsujack@icloud.com>
2024-03-27 15:34:14 +0900

    a

--- goodbyeUsb.ps1
+++ goodbyeUsb.ps1
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
+
 #vlcを終了
-=======
-	#vlcを終了
->>>>>>> 987f4d5120d80773bc4f885799c303a7c1e3308f
+
 	$pro = Get-process C:\Program Files\VLC\vlc.exe
 	if ( $pro -ne $null ) {
 	Stop-process -name C:\Program Files\VLC\vlc.exe
@@ -10,9 +8,7 @@
 
 
 Add-Type -Assembly System.Windows.Forms
-<<<<<<< HEAD
 [System.Windows.Forms.MessageBox]::Show("Good Bye Usb!!", "ハロー")
 =======
 [System.Windows.Forms.MessageBox]::Show("
 USB is disconnected. Please insert the USB.", "alert")
->>>>>>> 987f4d5120d80773bc4f885799c303a7c1e3308f
