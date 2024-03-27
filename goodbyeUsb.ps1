b6cb557645b982bc2efc0dce8f033d4e459801cc
github2022Oug <walnutr20912@gmail.com>
2024-03-27 13:58:09 +0900

    gixed

--- goodbyeUsb.ps1
+++ goodbyeUsb.ps1
@@ -1,10 +1,10 @@
-	#vlcを終了
-	$pro = Get-process C:\Program Files\VLC\vlc.exe
+	# # vlcを終了
+	Get-process "C:\Program Files\VLC\vlc.exe" | 2> Out-Null
 	if ( $pro -ne $null ) {
-	Stop-process -name C:\Program Files\VLC\vlc.exe
+	Stop-process -name "C:\Program Files\VLC\vlc.exe" | 2> Out-Null
 	}
 
 
-Add-Type -Assembly System.Windows.Forms
-[System.Windows.Forms.MessageBox]::Show("
-USB is disconnected. Please insert the USB.", "alert")
+	Add-Type -Assembly System.Windows.Forms
+	[System.Windows.Forms.MessageBox]::Show("
+	USB is disconnected. Please insert the USB.", "alert")
